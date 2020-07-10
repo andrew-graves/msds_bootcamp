f1cda579437b24661ca72499f64ed2fba4d14634
Andrew Graves <ajg3eh@virginia.edu>
2020-07-10 10:43:31 -0400

    removed extra empty lines

--- module_5/r/print_time.R
+++ module_5/r/print_time.R
@@ -1,3 +1,1 @@
 print(paste("The current time is", format(Sys.time(), "%H:%M")))
-
-      
--- module_5/r/validate_input.R
+++ module_5/r/validate_input.R
@@ -4,9 +4,10 @@ validate_input <- function(){
   
   input <- readline("Please type a word: ")
 
-while(!grepl("^[A-Za-z]+$", input)){
+  while(!grepl("^[A-Za-z]+$", input)){
   input <- readline("Your input was not a word... please try again: ")
-}
+  }
+  
   print("Your input was a word!")
 }
   
